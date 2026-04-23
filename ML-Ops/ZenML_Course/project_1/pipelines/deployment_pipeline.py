7d0bde47aa97f3300b4f69c3e193299404ac1c49
youssef-omarrr <youssefomer555@gmail.com>
2026-04-23 18:08:00 +0200

    worst mlops course ever

--- ML-Ops/ZenML_Course/project_1/pipelines/deployment_pipeline.py
+++ ML-Ops/ZenML_Course/project_1/pipelines/deployment_pipeline.py
@@ -1,84 +1,29 @@
 import json
-
-# from .utils import get_data_for_test
 import os
 
 import numpy as np
 import pandas as pd
-from materializer.custom_materializer import cs_materializer
-from steps.clean_data import clean_data
-from steps.evaluation import evaluation
-from steps.ingest_data import ingest_data
+
+from steps.clean_data import clean_df
+from steps.ingest_data import ingest_df
 from steps.model_train import train_model
-from zenml import pipeline, step
+from steps.evaluation import eval_model
+
+from zenml import step, pipeline
 from zenml.config import DockerSettings
 from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
-from zenml.integrations.constants import MLFLOW, TENSORFLOW
-from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import (
-    MLFlowModelDeployer,
-)
+
+from zenml.integrations.constants import MLFLOW
+from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import MLFlowModelDeployer
 from zenml.integrations.mlflow.services import MLFlowDeploymentService
 from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
-from zenml.steps import BaseParameters, Output
 
-from .utils import get_data_for_test
+
+import pandas as pd
+from pipelines.utils import cs_materializer
+
 
 docker_settings = DockerSettings(required_integrations=[MLFLOW])
-import pandas as pd
-
-# import os
-
-
-# from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import (
-#     MLFlowModelDeployer,
-# )
-# from zenml.integrations.mlflow.services import MLFlowDeploymentService
-# from zenml.pipelines import pipeline
-# from zenml.steps import BaseParameters, Output, step
-
-
-requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
-
-
-@step(enable_cache=False)
-def dynamic_importer() -> str:
-    """Downloads the latest data from a mock API."""
-    data = get_data_for_test()
-    return data
-
-
-class DeploymentTriggerConfig(BaseParameters):
-    """Parameters that are used to trigger the deployment"""
-
-    min_accuracy: float = 0.9
-
-
-@step
-def deployment_trigger(
-    accuracy: float,
-    config: DeploymentTriggerConfig,
-) -> bool:
-    """Implements a simple model deployment trigger that looks at the
-    input model accuracy and decides if it is good enough to deploy"""
-
-    return accuracy > config.min_accuracy
-
-
-class MLFlowDeploymentLoaderStepParameters(BaseParameters):
-    """MLflow deployment getter parameters
-
-    Attributes:
-        pipeline_name: name of the pipeline that deployed the MLflow prediction
-            server
-        step_name: the name of the step that deployed the MLflow prediction
-            server
-        running: when this flag is set, the step only returns a running service
-        model_name: the name of the model that is deployed
-    """
-
-    pipeline_name: str
-    step_name: str
-    running: bool = True
 
 
 @step(enable_cache=False)
@@ -153,50 +98,20 @@ def predictor(
     return prediction
 
 
-@step
-def predictor(
-    service: MLFlowDeploymentService,
-    data: str,
-) -> np.ndarray:
-    """Run an inference request against a prediction service"""
-
-    service.start(timeout=10)  # should be a NOP if already started
-    data = json.loads(data)
-    data.pop("columns")
-    data.pop("index")
-    columns_for_df = [
-        "payment_sequential",
-        "payment_installments",
-        "payment_value",
-        "price",
-        "freight_value",
-        "product_name_lenght",
-        "product_description_lenght",
-        "product_photos_qty",
-        "product_weight_g",
-        "product_length_cm",
-        "product_height_cm",
-        "product_width_cm",
-    ]
-    df = pd.DataFrame(data["data"], columns=columns_for_df)
-    json_list = json.loads(json.dumps(list(df.T.to_dict().values())))
-    data = np.array(json_list)
-    prediction = service.predict(data)
-    return prediction
-
-
 @pipeline(enable_cache=True, settings={"docker": docker_settings})
 def continuous_deployment_pipeline(
-    min_accuracy: float = 0.9,
+    min_accuracy: float = 0.7,
     workers: int = 1,
     timeout: int = DEFAULT_SERVICE_START_STOP_TIMEOUT,
 ):
     # Link all the steps artifacts together
-    df = ingest_data()
-    x_train, x_test, y_train, y_test = clean_data(df)
+    df = ingest_df()
+    x_train, x_test, y_train, y_test = clean_df(df)
     model = train_model(x_train, x_test, y_train, y_test)
-    mse, rmse = evaluation(model, x_test, y_test)
-    deployment_decision = deployment_trigger(accuracy=mse)
+    r2_score, rmse = eval_model(model, x_test, y_test)
+    
+    deployment_decision = True if r2_score>= min_accuracy else False
+    
     mlflow_model_deployer_step(
         model=model,
         deploy_decision=deployment_decision,
